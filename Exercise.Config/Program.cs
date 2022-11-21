7dc500e33af8e8dab2bbbe9f348e7f5f8dc8ad52
Davide Chiesa <davide_chiesa@hotmail.com>
2022-11-21 10:13:54 +0100

    -impostare configurazioni esterne

---
+++ Exercise.Config/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace Exercise.Config
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IConfiguration config = new ConfigurationBuilder().AddJsonFile("appconfig.json").Build();
+            Setting settings = config.GetRequiredSection("setting").Get<Setting>();
+            Console.WriteLine(settings.Username);
+            Console.WriteLine(settings.Password);
+        }
+    }
+
+    public class Setting
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
